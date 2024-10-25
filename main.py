1239099fcf89e410d83e5c3cef10201f304342b5
MyungKyuKim <MyungKyuKim>
2024-10-26 02:27:55 +0900

    주석 추가

--- main.py
+++ main.py
@@ -8,53 +8,61 @@ from utils.jaso_complition import join_jamos
 
 # 1. model load
 # 모델이 저장된 경로 (여기서는 best.pt를 사용)
-model_path = 'runs/detect/yolo_braille_hangeul_mkdata_only500e/weights/best.pt'
+model_path = '/home2/kimmg/alpha/runs/detect/yolo_braille_hangeul_mkdata_only300e/weights/best.pt'
 # model_path = YOUR_MODEL_PATH
 model = YOLO(model_path)  # 학습된 YOLO 모델 불러오기
 
 # 2. image input
-image_path = "test_image/mt.jpeg"
-image = cv2.imread(image_path)
+def braille_to_text(image_path):
+    image = cv2.imread(image_path)
 
-# 3. model output
-results = model(image)
+    # 3. model output
+    results = model(image)
 
-# 4. output(binary to jaso)
-# 예측 결과 
-# 바운딩 박스, confidence, 클래스
-# results[0].boxes.xyxy는 바운딩 박스 좌표, results[0].boxes.conf는 confidence score
-# results[0].boxes.cls는 클래스 예측
-boxes = results[0].boxes.xyxy.cpu().numpy()  # 바운딩 박스 좌표
-scores = results[0].boxes.conf.cpu().numpy()  # confidence score
-classes = results[0].boxes.cls.cpu().numpy()  # 클래스
+    # 4. output(binary to jaso)
+    # 예측 결과 
+    # 바운딩 박스, confidence, 클래스
+    # results[0].boxes.xyxy는 바운딩 박스 좌표, results[0].boxes.conf는 confidence score
+    # results[0].boxes.cls는 클래스 예측
+    boxes = results[0].boxes.xyxy.cpu().numpy()  # 바운딩 박스 좌표
+    scores = results[0].boxes.conf.cpu().numpy()  # confidence score
+    classes = results[0].boxes.cls.cpu().numpy()  # 클래스
 
-# 예측 결과 이미지 위에 표시
-binary_output = []
-for box, score, cls in zip(boxes, scores, classes):
-    if score > 0.1:
-        x1, y1, x2, y2 = map(int, box)
-        label = f"{model.names[int(cls)]}: {score:.2f}"
-        cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)  # 바운딩 박스 그리기
-        cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    # 예측 결과 이미지 위에 표시
+    binary_output = []
+    for box, score, cls in zip(boxes, scores, classes):
+        if score > 0.1:
+            x1, y1, x2, y2 = map(int, box)
+            label = f"{model.names[int(cls)]}: {score:.2f}"
+            cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)  # 바운딩 박스 그리기
+            cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
-        binary_output.append((x1, model.names[int(cls)]))
-
-
-binary = []
-
-binary_output.sort(key=lambda x:x[0]) # x1값을 기준으로 오름차순 sorting
-for _,b in binary_output:
-    binary.append(b)
+            binary_output.append((x1, model.names[int(cls)]))
 
 
+    binary = []
 
-#예측 결과 이미지 저장
-output_image_path = "test_output/mtn2.jpg"
-cv2.imwrite(output_image_path, image)
+    binary_output.sort(key=lambda x:x[0]) # x1값을 기준으로 오름차순 sorting
+    for _,b in binary_output:
+        binary.append(b)
 
-# 5. jaso complition and print
-result = jaso2char(binary)
-print(result)
-combine_result = join_jamos(result)
-print(combine_result)
 
+
+    #예측 결과 이미지 저장
+    output_image_path = "/home2/kimmg/alpha/test_output/te.jpg"
+    cv2.imwrite(output_image_path, image)
+
+    # 5. jaso complition and print
+    result = jaso2char(binary)
+    print(result)
+    combine_result = join_jamos(result)
+    return combine_result
+
+image_folder = "/home2/kimmg/alpha/dataset/testmk2datset/train/images"
+folder = os.listdir("/home2/kimmg/alpha/dataset/testmk2datset/train/images")
+a = []
+for i in folder:
+    image = image_folder+'/'+i
+    a.append(braille_to_text(image))
+for j in a:
+    print(j)
--- utils/binary_to_jaso.py
+++ utils/binary_to_jaso.py
@@ -9,52 +9,57 @@ from utils.binary_jaso_mapping import (CHOSEONG,
                                 )
 from typing import List
 
-def jaso2char(l: List)->str:
+def jaso2char(l: List) -> str:
     result = []
     skip_next = False  # 다음 원소를 건너뛸지 여부를 저장하는 플래그
     print(l)
     for m in range(len(l)):
-        if skip_next:
-            skip_next = False  # 이전에 m+1을 건너뛰기로 했으면 이번 루프는 처리하지 않고 넘김
+        if skip_next: # skip_next True 이면 이번 루프는 처리하지 않고 넘김
+            skip_next = False  
             continue
 
-        if l[m] in CHOSEONG:
-            if m + 1 < len(l) and l[m] == '000001' and l[m+1] in CHOSEONG:
-                result.append(DOUBLE[l[m+1]])
-                skip_next = True  # 다음 원소를 건너뛰기 위해 플래그 설정
-            elif m + 1 < len(l) and l[m+1] in DOUBLE:  # 된소리(초성) 처리
-                combined_s = l[m]  
-                result.append(DOUBLE[combined_s])  
-                skip_next = True  # 다음 원소를 건너뛰기 위해 플래그 설정
+        try:
+            if l[m] in CHOSEONG: # <============ 현재 들어온 자소가 초성(자음자)일 경우 ===============>
+                if m + 1 < len(l) and l[m] == '000001' and l[m+1] in CHOSEONG: # 만약 초성 'ㅅ'이 오게되면, 'ㅅ' 바로 다음으로 오는 자소가 초성인지 검사 - 한글점자규정해설서 3항 
+                    result.append(DOUBLE[l[m+1]])
+                    skip_next = True  # 다음 원소를 건너뛰기 위해 플래그 설정
+
+                elif m + 1 < len(l) and l[m+1] in DOUBLE:  # 된소리(초성) 처리, 초성글자 뒤에 초성이 오게 되면, 해당 자소와 다음 자소를 검사하고 해싱되는 키 검색 - 한글 점자 규정 해설서 5항 된소리 처리
+                    combined_s = l[m]  
+                    result.append(DOUBLE[combined_s])  # 키에 해당하는 값으로 변경.
+                    skip_next = True  # 다음 원소를 건너뛰기 위해 플래그 설정
+                    
+                elif m + 1 < len(l) and l[m] in SHORT_WORD and (l[m+1] in JONGSEONG or l[m+1] in CHOSEONG):  # 약어 처리(해당 초성이 약어에 해당되고, 뒤에 종성이나 초성이 올 경우 -> 중성 자소 생략을 의미함.) 한글점자규정해설서 제2장 6절, 12항 - 
+                    short_word = l[m]
+                    result += SHORT_WORD[short_word] # 약어 키에 해당하는 값으로 해싱후 추가
+                else:
+                    result.append(CHOSEONG[l[m]]) # 위의 조건식을 모두 통과할 경우 단순 초성으로 처리.
+
+            elif l[m] in JUNGSEONG:# <============ 현재 들어온 자소가 중성(모음자)일 경우 ===============>
+                if (0 < m - 1 or m == 0 ) and l[m-1] not in CHOSEONG: # 'ㅇ' 이 단어의 가장 첫 초성으로 올경우, 중성이 어두에 올 경우
+                    result.append('ㅇ') # 'ㅇ'을 중성 앞에 추가한다. 
                 
-            elif m + 1 < len(l) and l[m] in SHORT_WORD and (l[m+1] in JONGSEONG or l[m+1] in CHOSEONG):  # 약어 처리
-                short_word = l[m]
-                result += SHORT_WORD[short_word]
-            else:
-                result.append(CHOSEONG[l[m]])
-
-        elif l[m] in JUNGSEONG:
-            if (0 < m - 1 or m == 0 )and l[m-1] not in CHOSEONG: # 'ㅇ' 이 초성으로 올경우 추가 처리
-                result.append('ㅇ')
-            
-            if m + 1 < len(l) and l[m+1] == '111010':  # 이중 모음 체크
-                result.append(DOUBLE_JUNGSUNG[l[m]])
-                skip_next = True  # 다음 원소를 건너뛰기 위해 플래그 설정
-            else:
-                result.append(JUNGSEONG[l[m]])
-
-        elif l[m] in JONGSEONG:
-            if m + 1 < len(l) and l[m+1] in JONGSEONG:
-                combined_e = l[m] + l[m+1]
-                if combined_e in DOUBLE_JONGSUNG:
-                    result.append(DOUBLE_JONGSUNG[combined_e])
+                if m + 1 < len(l) and l[m+1] == '111010':  # 이중 모음 체크, 한글 점자규정 1장 3절 7항
+                    result.append(DOUBLE_JUNGSUNG[l[m]])
                     skip_next = True  # 다음 원소를 건너뛰기 위해 플래그 설정
                 else:
-                    result.append(JONGSEONG[l[m]])
-            else:
-                result.append(JONGSEONG[l[m]])
+                    result.append(JUNGSEONG[l[m]]) # 이전 조건들에 해당되지 않으면 일반 중성(모음자)로 처리
 
-        elif l[m] in SHORT_DIRECT_MAPPING:
-            result += SHORT_DIRECT_MAPPING[l[m]]
+            elif l[m] in JONGSEONG:# <============ 현재 들어온 자소가 종성(받침자)일 경우 ===============>
+                if m + 1 < len(l) and l[m+1] in JONGSEONG: # 해당 자소가 겹받침이 될 가능성이 있는 경우를 검사 한글 점자규정 (1장 2절 5항)
+                    combined_e = l[m] + l[m+1] # 다음 자소까지 고려
+                    if combined_e in DOUBLE_JONGSUNG: # 만약 겹받침이 가능한 자소이고, 해당 자소가 겹받침일 경우
+                        result.append(DOUBLE_JONGSUNG[combined_e])
+                        skip_next = True  # 다음 원소를 건너뛰기 위해 플래그 설정
+                    else:
+                        result.append(JONGSEONG[l[m]])
+                else:
+                    result.append(JONGSEONG[l[m]])
+
+            elif l[m] in SHORT_DIRECT_MAPPING: # <============ 현재 들어온 자소가 부가적인 요소를 고려하지 않아도 되는 약어일 경우 ===============>
+                result += SHORT_DIRECT_MAPPING[l[m]] # 한글점자규정 2장 6절 12항의 표 2행 이후 처리
+        
+        except KeyError:
+            continue  # KeyError가 발생하면 무시하고 다음 단계로 넘어감
     
     return result
